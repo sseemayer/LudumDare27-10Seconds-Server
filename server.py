004a80bffa9e294f0d21f7b74eb97c40f2d947d1
Stefan Seemayer <stefan@seemayer.de>
2013-08-24 08:56:00 +0200

    Initial commit

---
+++ server.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask
+from flask.ext.heroku import Heroku
+from flask.ext.sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+heroku = Heroku(app)
+db = SQLAlchemy(app)
+
+@app.route('/')
+def hello():
+    return 'Nothing to see here, move along!'
