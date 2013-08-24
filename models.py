fd1f249c24fa21c617a668377c9cee0cec902fd2
Stefan Seemayer <stefan@seemayer.de>
2013-08-24 09:06:15 +0200

    Add color, highscore model

--- models.py
+++ models.py
@@ -4,6 +4,7 @@ class Replay(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     player = db.Column(db.String(255))
+    color = db.Column(db.String(7))
     challenge = db.Column(db.String(80))
 
     replay = db.Column(db.Text)
@@ -16,3 +17,12 @@ class Replay(db.Model):
 
     def __repr__(self):
         return '<Replay {0}: {1} on {2}>'.format(self.id, self.player, self.challenge)
+
+
+class Highscore(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    player = db.Column(db.String(255))
+    color = db.Column(db.String(7))
+
+    score = db.Column(db.Integer)
